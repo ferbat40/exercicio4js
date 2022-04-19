3f2d723b540620fabc816e89821797837126cf25
Fernando <setec.solda@gmail.com>
2022-04-19 16:44:24 -0300

    at

--- Posto.ts
+++ Posto.ts
@@ -43,15 +43,14 @@ export class Posto{
     }
 
 
-    sugereCombustivel(){
-     
-       if ( (this._etanol.preco/this._gasolina.preco)<0.70){
-         console.log(`Etanol no valor de ${this._etanol.preco} e mais vantajoso`);
-        }
-       else{
-        console.log(`Gasolina no valor de ${this._gasolina.preco} e mais vantajoso`);
-       }
-        }
+    sugereCombustivel() {
+      if ((this._etanol.preco / this._gasolina.preco) < 0.70) {
+          console.log(`${this._etanol.tipo} no valor de ${this._etanol.preco} e mais vantajoso em relacao a ${this._gasolina.tipo} de ${this._gasolina.preco}`);
+      }
+      else {
+          console.log(`${this._gasolina.tipo} no valor de ${this._gasolina.preco} e mais vantajoso em relacao ao ${this._etanol.tipo} de ${this._etanol.preco}`);
+      }
+  }
 
     
 
