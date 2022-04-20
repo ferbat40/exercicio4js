c787f97ee2559d62ac8ca00798f6c4deced78cfc
Fernando <setec.solda@gmail.com>
2022-04-19 22:41:05 -0300

    alter

--- 2/index.js
+++ 2/index.js
@@ -2,23 +2,23 @@ import { Combustivel } from "./Combustivel.js";
 import { Posto } from "./Posto.js";
 
 
-const cetanol = 'etanol';
-const cgasolina = 'gasolina';
+const tetanol = 'etanol';
+const tgasolina = 'gasolina';
 
-let etanol = new Combustivel(cetanol,4.15);
-let gasolina = new Combustivel(cgasolina,7.40);
+let etanol = new Combustivel(tetanol,5.15);
+let gasolina = new Combustivel(tgasolina,7.40);
 let posto = new Posto('petrobras',gasolina,etanol);
 posto.sugereCombustivel();
 
 
-etanol = new Combustivel(cetanol,4.35);
-gasolina = new Combustivel(cgasolina,7.30);
+etanol = new Combustivel(tetanol,5.35);
+gasolina = new Combustivel(tgasolina,7.30);
 posto = new Posto('ipiranga',gasolina,etanol);
 posto.sugereCombustivel();
 
 
-etanol = new Combustivel(cetanol,4.15);
-gasolina = new Combustivel(cgasolina,7.20);
+etanol = new Combustivel(tetanol,4.15);
+gasolina = new Combustivel(tgasolina,7.20);
 posto = new Posto('shell',gasolina,etanol);
 posto.sugereCombustivel();
 
